55d8fe1a4de9a27f5b14290fac411061cf6bb189
1106137210@nkust.edu.tw <zhang219>
2019-10-31 23:34:11 +0800

    新增 heartbeat 主程式

--- 20191024/練習區/control.js
+++ 20191024/練習區/control.js
@@ -3,3 +3,23 @@ function getColor() {
 	document.documentElement.style.setProperty("--back_color",newColor)
 }
 
+function beat() {
+	if(typeof(Storage) !== "undefined") {
+		if (sessionStorage.clickcount) {
+			var count = Number(sessionStorage.clickcount);
+			if (count > 9) {
+				count = 1;
+			} else {
+				count = count + 1;
+			}
+		} else {
+			count = 1;
+		}
+		sessionStorage.clickcount = count;
+		var beatRate = (1/count).toString() + "s";
+		document.documentElement.style.setProperty("--heartbeat",beatRate);
+		document.getElementById("heart").innerHTML = sessionStorage.clickcount;
+	} else {
+		document.getElementById("heart").innerHTML = "Sorry, your browser does not support web storage...";
+	}
+}
