f7cd59e259c0f835d3e174d9fa41f93bf98bf21b
1106137210@nkust.edu.tw <zhang219>
2019-10-31 21:49:32 +0800

    Python 抓取 JSON 資料

--- 20191031/練習區/py_getJSON.py
+++ 20191031/練習區/py_getJSON.py
@@ -3,7 +3,7 @@
 
 import requests
 import json
-data = requests.get(url="http://cloud.culture.tw/frontsite/trans/SearchShowAction.do?method=doFindTypeJ&category=3")
-with open("music.json","w",encoding="utf-8") as myFile:
+data = requests.get(url="https://cloud.culture.tw/frontsite/trans/SearchShowAction.do?method=doFindTypeJ&category=17")
+with open("concert.json","w",encoding="utf-8") as myFile:
     json.dump(data.json(), myFile,ensure_ascii=False)
 myFile.close()
